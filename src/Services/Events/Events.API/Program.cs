7e9fda83ebc259778e559af76d000167826b0ec6
Ping Dong <Ping.Dong@live.com>
2018-08-05 11:51:40 +1200

    Refactor multiple configuration sources

--- src/Services/Events/Events.API/Program.cs
+++ src/Services/Events/Events.API/Program.cs
@@ -25,13 +25,56 @@ namespace PingDong.Newmoon.Events
         public static void Main(string[] args)
         {
             var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-            var configuration = new ConfigurationBuilder()
-                    .SetBasePath(Directory.GetCurrentDirectory())
-                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                    .AddJsonFile($"appsettings.{env}.json", optional: true)
-                    .AddCommandLine(args)
-                    .AddEnvironmentVariables()
-                    .Build();
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                // Application related setting
+                //   for example: log setting
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                // If the same settings in appsettings.json needs to be replaced in development environment, write here
+                //   for example: different logging setting
+                .AddJsonFile($"appsettings.{env}.json", optional: true)
+                // Some secrets that used in Development could save in UserSecrects
+                //   For example: you probably don't want to share you database credential,
+                //                if you want to share your codes.
+                // https://docs.microsoft.com/en-us/aspnet/core/security/app-secrets
+                //     dotnet user-secrets set "Database:DbPassword" "pass123"
+                //     dotnet user-secrets list
+                //     dotnet user-secrets remove "Database:DbPassword"
+                //     dotnet user-secrets clear
+                //
+                // Or in Visual Studio
+                // Right click on the target project, then click 'Manage User Secrets'
+                .AddUserSecrets<Startup>();
+
+            if (env != "Development")
+            {
+                var endpoint = Environment.GetEnvironmentVariable("Azure:Vault:Endpoint");
+                var clientId = Environment.GetEnvironmentVariable("Azure:Vault:ClientId");
+                var clientSecret = Environment.GetEnvironmentVariable("Azure:Vault:ClientSecret");
+
+                // Secrets should save in vault in production environment
+                //   config need to be built to retrieve Vault information
+                //  
+                //      DbConnectionString in production
+                if (!string.IsNullOrWhiteSpace(endpoint) && !string.IsNullOrWhiteSpace(clientId) && !string.IsNullOrWhiteSpace(clientSecret))
+                {
+                    builder.AddAzureKeyVault(
+                        $"https://{endpoint}.vault.azure.net/",
+                        clientId,
+                        clientSecret);
+                }
+
+            }
+
+            builder
+                // Environment-aware settings
+                //   for example: external service uri
+                .AddCommandLine(args)
+                .AddEnvironmentVariables();
+
+            var configuration = builder.Build();
+
             Log.Logger = new LoggerConfiguration()
                 .ReadFrom.Configuration(configuration)
                 .CreateLogger();
@@ -40,7 +83,8 @@ namespace PingDong.Newmoon.Events
             {
                 Log.Information("Starting web host for Event Services");
 
-                var host = BuildWebHost(args).Build();
+                var host = BuildWebHost(args, configuration).Build();
+
                 host.MigrateDbContext<EventContext>((context, services) =>
                     {
                         var logger = services.GetService<ILogger<EventContextSeed>>();
@@ -73,78 +117,30 @@ namespace PingDong.Newmoon.Events
         /// Web host building
         /// </summary>
         /// <param name="args">args</param>
+        /// <param name="configuration">configuration</param>
         /// <returns>Web host</returns>
-        public static IWebHostBuilder BuildWebHost(string[] args) =>
+        public static IWebHostBuilder BuildWebHost(string[] args, IConfiguration configuration) =>
             // Details
             // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/host/web-host?view=aspnetcore-2.1
 
             WebHost.CreateDefaultBuilder(args)
-                .CaptureStartupErrors(true)
-                .UseKestrel()
-                // Application Configure
-                .ConfigureAppConfiguration((builderContext, config) =>
-                {
-                    var env = builderContext.HostingEnvironment;
-
-                    config.SetBasePath(Directory.GetCurrentDirectory());
-                        
-                    config
-                        // Application related setting
-                        //   for example: log setting
-                        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                        // If the same settings in appsettings.json needs to be replaced in development environment, write here
-                        //   for example: different logging setting
-                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)     
-                        
-                        // Environment-aware settings
-                        //   for example: external service uri
-                        .AddCommandLine(args)
-                        .AddEnvironmentVariables();
-
-                    if (env.IsDevelopment())
-                    {
-                        // Some secrets that used in Development could save in UserSecrects
-                        //   For example: you probably don't want to share you database credential,
-                        //                if you want to share your codes.
-                        // https://docs.microsoft.com/en-us/aspnet/core/security/app-secrets
-                        //     dotnet user-secrets set "Database:DbPassword" "pass123"
-                        //     dotnet user-secrets list
-                        //     dotnet user-secrets remove "Database:DbPassword"
-                        //     dotnet user-secrets clear
-                        //
-                        // Or in Visual Studio
-                        // Right click on the target project, then click 'Manage User Secrets'
-                        config.AddUserSecrets<Startup>();
-                    }
-                    // Secrets should save in vault in production environment
-                    //   config need to be built to retrieve Vault information
-                    //  
-                    //      DbConnectionString in production
-                    //
-                    //else if (env.IsProduction())
-                    //{
-                    //    var vault = new ConfigurationBuilder();
-                    //    vault.AddAzureKeyVault(
-                    //        $"https://{config["AzureVault:VaultName"]}.vault.azure.net/",
-                    //        config["AzureVault:ClientId"],
-                    //        config["AzureVault:ClientSecret"]);
-
-                    //    var vaultConfig = vault.Build();
-
-                    //    config.AddConfiguration(vaultConfig);
-                    //}
-
-                    config.Build();
-                })
-                // Autofac intialise
-                .ConfigureServices(services => services.AddAutofac())
-                // HealthChecks initialise
-                .UseHealthChecks("/health")
-                // Application Insights initialise
-                .UseApplicationInsights()
-                // Serilog initialise
-                .UseSerilog((context, config) => { config.ReadFrom.Configuration(context.Configuration); })
-                // Starting
-                .UseStartup<Startup>();
+                    .CaptureStartupErrors(true)
+                    .UseKestrel()
+                    // Application Configure
+                    .ConfigureAppConfiguration((builderContext, config) =>
+                        {
+                            config.AddConfiguration(configuration);
+                        })
+                    .UseContentRoot(Directory.GetCurrentDirectory())
+                    // Autofac intialise
+                    .ConfigureServices(services => services.AddAutofac())
+                    // HealthChecks initialise
+                    .UseHealthChecks("/health")
+                    // Application Insights initialise
+                    .UseApplicationInsights()
+                    // Serilog initialise
+                    .UseSerilog((context, config) => { config.ReadFrom.Configuration(context.Configuration); })
+                    // Starting
+                    .UseStartup<Startup>();
     }
 }
