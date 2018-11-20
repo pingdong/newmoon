110183e38161aa075cfbf2bc79e42a950be1c2f5
Ping Dong <Ping.Dong@live.com>
2018-11-20 22:01:23 +1300

    Preloading lazy module

--- src/App/ng-newmoon/src/app/app-routing.module.ts
+++ src/App/ng-newmoon/src/app/app-routing.module.ts
@@ -10,13 +10,12 @@ const appRoutes: Routes = [
   // Popup, second outlet
   { path: 'message', component: PopupMessageComponent, outlet: 'popup', canActivate: [AuthGuard] },
 
+  // Preloading lazy module
+  // { path: 'lazy-module', loadChildren: './features/lazy/lazy.module#LazyModule', data: { preload: true }},
+  // If any guard is set to lazy module, event preload is true, the lazy module won't be preloaded
   { path: 'dashboard', loadChildren: './features/dashboard/dashboard.module#DashboardModule'},
   { path: 'setting', loadChildren: './features/app-setting/app-setting.module#AppSettingModule', canLoad: [AuthGuard] },
-  { path: 'user-profile',
-    loadChildren: './features/user-profile/user-profile.module#UserProfileModule',
-    data: { preload: true },
-    canLoad: [AuthGuard]
-  },
+  { path: 'user-profile', loadChildren: './features/user-profile/user-profile.module#UserProfileModule', canLoad: [AuthGuard] },
 
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent },
