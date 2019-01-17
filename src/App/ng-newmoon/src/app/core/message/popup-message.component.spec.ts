2ee2f4bb65880377f84cbe0a3c1d004e6ac0044e
Ping Dong <ping.dong@live.com>
2019-01-17 21:23:54 +1300

    tslint

--- src/App/ng-newmoon/src/app/core/message/popup-message.component.spec.ts
+++ src/App/ng-newmoon/src/app/core/message/popup-message.component.spec.ts
@@ -1,11 +1,12 @@
 import { Router } from '@angular/router';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
 
 import { PopupMessageComponent } from './popup-message.component';
 
 describe('Popup Message', () => {
 
   const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+  routerSpy.navigate.and.stub();
 
   let component: PopupMessageComponent;
   let fixture: ComponentFixture<PopupMessageComponent>;
@@ -26,8 +27,21 @@ describe('Popup Message', () => {
     fixture.detectChanges();
   });
 
+  it('should call router in send() after 1ms', fakeAsync(() => {
+    routerSpy.navigate.calls.reset();
+
+    component.send();
+
+    fixture.detectChanges();
+
+    expect(routerSpy.navigate.calls.count()).toEqual(0);
+
+    tick(1000);
+    expect(routerSpy.navigate.calls.count()).toEqual(1);
+  }));
+
   it('should call router in Close()', () => {
-    routerSpy.navigate.and.stub();
+    routerSpy.navigate.calls.reset();
 
     component.close();
 
