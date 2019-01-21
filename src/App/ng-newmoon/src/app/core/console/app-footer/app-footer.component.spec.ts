4275da00df1b3b72e5760c2b7997babf70795ca8
Ping Dong <ping.dong@live.com>
2019-01-21 20:47:31 +1300

    app-footer unit testing

--- src/App/ng-newmoon/src/app/core/console/app-footer/app-footer.component.spec.ts
+++ src/App/ng-newmoon/src/app/core/console/app-footer/app-footer.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { AppFooterComponent } from './app-footer.component';
 
@@ -13,13 +14,23 @@ describe('AppFooterComponent', () => {
     .compileComponents();
   }));
 
-  beforeEach(() => {
+  beforeEach(async(() => {
     fixture = TestBed.createComponent(AppFooterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should has correct year', () => {
+    const currentYear = (new Date()).getFullYear();
+
+    expect(component.currentYear).toBe(currentYear, 'Value doesn\'t match');
+
+    const copyright = fixture.debugElement.query(By.css('.footer__copyright'));
+    expect(copyright.nativeElement.innerText).toEqual(`Ping Dong © 2018 - ${currentYear}`, 'Copyright doesn\'t match its expectation');
+  });
+
 });
